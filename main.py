cb37b61fb57f9b2f2747d98162d2f7def344107b
fjelenic <70028296+fjelenic@users.noreply.github.com>
2020-10-31 18:04:08 +0100

    Fixed output

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ t1 = time()
 POPULATION_SIZE = 5
 N_EVALUATION_STEPS = 10000
 N_EPOCHS = 100000
-STOP_CRITERION = 1000
+STOP_CRITERION = 750
 
 population = Population(POPULATION_SIZE,N_EVALUATION_STEPS)
 best = (population.get_best().score,0)
@@ -23,7 +23,7 @@ for i in range(N_EPOCHS):
     if temp_best[0] < best[0]:
         best = temp_best
     if i % 10 == 0:
-        print("Epoch {}. best score: {}".format(i,temp_best))
+        print("Epoch {}. best score: {}".format(i,temp_best[0]))
 
 population.get_best().to_file("best_strategy.txt")
 
