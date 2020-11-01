df21dc1a218aa240d5af2630e025ffdfb307d267
fjelenic <70028296+fjelenic@users.noreply.github.com>
2020-11-01 21:36:45 +0100

    update

--- main.py
+++ main.py
@@ -1,32 +1,35 @@
-from Evolutionary_algorithm import Population
+from Evolutionary_algorithm import *
 from time import time
 
 t1 = time()
 
-POPULATION_SIZE = 5
-N_EVALUATION_STEPS = 10000
-N_EPOCHS = 100000
-STOP_CRITERION = 750
+POPULATION_SIZE = 400
+N_EVALUATION_STEPS = 100000
+N_EPOCHS = 500
+STOP_CRITERION = 20
+MUTATION_CHANCE = 0.01
+ELITISM_PERCENT = 0.1
+TOURNAMENT_SIZE = 4
+setMutationChance(MUTATION_CHANCE)
 
-population = Population(POPULATION_SIZE,N_EVALUATION_STEPS)
-best = (population.get_best().score,0)
+population = Population(POPULATION_SIZE,N_EVALUATION_STEPS,TOURNAMENT_SIZE,ELITISM_PERCENT)
+population.initialise()
+best = (population.get_best(),0)
+print("Epoch 0. best score: {}, similarity to optimal: {}".format(0,best[0].score,best[0].like_optimal()))
+
 
 for i in range(N_EPOCHS):
     if i - best[1] >= STOP_CRITERION:
         break
-    crossing_candidates = population.get_crossing_candidates()
-    new_unit = crossing_candidates[0].cross(crossing_candidates[1])
-    new_unit.mutate().evaluate(N_EVALUATION_STEPS)
-    mini_pop = crossing_candidates + [new_unit]
-    population.add(mini_pop)
-    temp_best = (population.get_best().score, i)
-    if temp_best[0] < best[0]:
+    population = population.next_generation()
+    temp_best = (population.get_best(), i)
+    if temp_best[0].score > best[0].score:
         best = temp_best
-    if i % 10 == 0:
-        print("Epoch {}. best score: {}".format(i,temp_best[0]))
+    print("Epoch {}. best score: {}, similarity to optimal: {}".format(i+1,temp_best[0].score,temp_best[0].like_optimal()))
 
 population.get_best().to_file("best_strategy.txt")
-
+print("Best strategy found scored: {}".format(population.get_best().score))
 print(time()-t1)
 
 
+print("Expected: {}".format(((1/3)*23*10 + (1/4)*10*10)/((23+10)*10)))
