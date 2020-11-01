5eee98542d6392028ae8b294812c89e16ba7bf11
fjelenic <70028296+fjelenic@users.noreply.github.com>
2020-11-01 21:35:08 +0100

    Added tournament selection

--- Evolutionary_algorithm.py
+++ Evolutionary_algorithm.py
@@ -6,7 +6,7 @@ MUTATION_CHANCE = 0.05
 class Unit:
 
     def __init__(self, strategy=None):
-        if not strategy:
+        if strategy is None:
             a = np.random.randint(3, size=(23, 10))
             b = np.random.randint(4, size=(10, 10))
             c = np.append(a, b, 0)
@@ -46,7 +46,7 @@ class Unit:
                         self.strategy[i][j] = new_action
         return self
 
-    def cross(self, other, method="uniform"):
+    def cross(self, other, method="elitist"):
         if method == "uniform":
             break_point = 0.5
         if method == "elitist":
@@ -70,37 +70,107 @@ class Unit:
         with open(file, 'w') as f:
             f.write(str(self))
 
+    def like_optimal(self):
+        opt = Unit(np.array([[0,0,0,0,0,0,0,0,0,0],
+                             [0,0,0,0,0,0,0,0,0,0],
+                             [0,0,0,0,0,0,0,0,0,0],
+                             [0,0,0,0,0,0,0,0,0,0],
+                             [0,2,2,2,2,0,0,0,0,0],
+                             [2,2,2,2,2,2,2,2,0,0],
+                             [2,2,2,2,2,2,2,2,2,0],
+                             [0,0,1,1,1,0,0,0,0,0],
+                             [1,1,1,1,1,0,0,0,0,0],
+                             [1,1,1,1,1,0,0,0,0,0],
+                             [1,1,1,1,1,0,0,0,0,0],
+                             [1,1,1,1,1,0,0,0,0,0],
+                             [1,1,1,1,1,1,1,1,1,1],
+                             [1,1,1,1,1,1,1,1,1,1],
+                             [1,1,1,1,1,1,1,1,1,1],
+                             [0,0,0,2,2,0,0,0,0,0],
+                             [0,0,0,2,2,0,0,0,0,0],
+                             [0,0,2,2,2,0,0,0,0,0],
+                             [0,0,2,2,2,0,0,0,0,0],
+                             [0,2,2,2,2,0,0,0,0,0],
+                             [1,2,2,2,2,1,1,0,0,0],
+                             [1,1,1,1,1,1,1,1,1,1],
+                             [1,1,1,1,1,1,1,1,1,1],
+                             [3,3,3,3,3,3,0,0,0,0],
+                             [3,3,3,3,3,3,0,0,0,0],
+                             [0,0,0,3,3,0,0,0,0,0],
+                             [2,2,2,2,2,2,2,2,0,0],
+                             [3,3,3,3,3,0,0,0,0,0],
+                             [3,3,3,3,3,3,0,0,0,0],
+                             [3,3,3,3,3,3,3,3,3,3],
+                             [3,3,3,3,3,1,3,3,1,1],
+                             [1,1,1,1,1,1,1,1,1,1],
+                             [3,3,3,3,3,3,3,3,3,3]]))
+        summation = 0
+        for i in range(opt.strategy.shape[0]):
+            for j in range(opt.strategy.shape[1]):
+                if opt[i][j] == self[i][j]:
+                    summation += 1
+        return summation/((23+10)*10)
+
 
 
 class Population:
 
-    def __init__(self, size, n_iterations):
+    def __init__(self, size, n_iterations, tournament_size, elitism_percent):
         self.members=[]
-        for i in range(size):
-            self.members.append(Unit().evaluate(n_iterations))
         self.size = size
+        self.n_iterations = n_iterations
+        self.tournament_size = tournament_size
+        self.elitism_percent = elitism_percent
 
     def __getitem__(self, item):
         return self.members[item]
 
-    def get_crossing_candidates(self):
-        candidates = set()
-        size = len(candidates)
-        while size < 2:
-            index = np.random.randint(len(self.members))
-            candidates.add(self[index])
-            if len(candidates) > size:
-                size = len(candidates)
-                del self.members[index]
-        return list(candidates)
+    def initialise(self):
+        for i in range(self.size):
+            self.add(Unit().evaluate(self.n_iterations))
 
-    def add(self,new_units):
-        new_units.sort(reverse = True, key=lambda x:x.score)
-        self.members += new_units[:-1]
+    def tournament_selection(self, N):
+        candidates = set()
+        while len(candidates) < N:
+            index = np.random.randint(self.size)
+            candidates.add(self[index])
+        return max(candidates, key=lambda x:x.score)
+
+    def add(self, unit):
+        lo = 0
+        hi = len(self.members)
+        if hi == 0:
+            return self.members.append(unit)
+        if hi == 1:
+            if unit.score > self[0].score:
+                self.members = [unit] + self.members
+            else:
+                self.members.append(unit)
+            return
+        while lo < hi:
+            mid = (lo + hi) // 2
+            if unit.score > self.members[mid].score and unit.score < self.members[mid-1].score:
+                break
+            if unit.score > self.members[mid].score:
+                hi = mid
+            else:
+                lo = mid + 1
+        self.members = self.members[:mid] + [unit] + self.members[mid:]
         return self
 
+    def next_generation(self):
+        new_population = Population(self.size,self.n_iterations,
+                                    self.tournament_size,self.elitism_percent)
+        new_population.members = self.members[:round(self.size*self.elitism_percent)]
+        while len(new_population.members) < self.size:
+            unit1 = self.tournament_selection(self.tournament_size)
+            unit2 = self.tournament_selection(self.tournament_size)
+            new_unit = unit1.cross(unit2).evaluate(self.n_iterations)
+            new_population.add(new_unit)
+        return new_population
+
     def get_best(self):
-        return max(self.members, key=lambda x:x.score)
+        return self.members[0]
 
 
 
